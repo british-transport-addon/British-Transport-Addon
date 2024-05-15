afbd9d7a7f8213d55f5ec15382854e9eaa12e485
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2024-05-15 17:25:25 +0100

    Added Architectury Template

---
+++ common/src/main/java/org/eu/awesomekalin/mtb/ExampleMod.java
@@ -0,0 +1,9 @@
+package org.eu.awesomekalin.mtb;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "mtb";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
