c8687e1f2d8eae21ddc42bc4a7ad149e9a89d072
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2024-06-02 14:13:01 +0100

    Added Greek Platform End Block

--- fabric/src/main/java/org/eu/awesomekalin/jta/mod/blocks/GreekPlatformEnd.java
+++ fabric/src/main/java/org/eu/awesomekalin/jta/mod/blocks/GreekPlatformEnd.java
@@ -1,9 +1,15 @@
 package org.eu.awesomekalin.jta.mod.blocks;
 
+import org.mtr.mapping.holder.*;
 import org.mtr.mapping.mapper.DoorBlockExtension;
 
 public class GreekPlatformEnd extends DoorBlockExtension {
     public GreekPlatformEnd() {
         super(true, blockSettings -> blockSettings.strength(4.0f).nonOpaque());
+    }
+
+    @Override
+    public void onPlaced2(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack itemStack) {
+
     }
 }
