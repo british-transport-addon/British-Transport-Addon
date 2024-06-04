0fe796e0ff7ebd42568fb2284b1859e20a77d930
Awesome Kalin <47223207+AwesomeKalin@users.noreply.github.com>
2024-06-04 14:17:01 +0000

    Start adding Rusty No Tresspass Block
    Doing in GH Codespaces, so it is not complete

---
+++ fabric/src/main/java/org/eu/awesomekalin/jta/mod/tools/IBlock.java
@@ -0,0 +1,7 @@
+package org.eu.awesomekalin.jta.mod.tools;
+
+import org.mtr.mapping.holder.*;
+
+public interface IBlock {
+    EnumProperty<DoubleBlockHalf> HALF = EnumProperty.of("half", DoubleBlockHalf.class);
+}
