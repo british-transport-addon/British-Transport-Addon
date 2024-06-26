bdb6eafd115b8bda8fc096f96d10ad8f1e6a7fb6
dashkt <contact.pear.casa@gmail.com>
2024-06-26 07:57:40 +0100

    rawr

--- fabric/src/main/java/org/eu/awesomekalin/jta/mod/InitClient.java
+++ fabric/src/main/java/org/eu/awesomekalin/jta/mod/InitClient.java
@@ -2,10 +2,7 @@ package org.eu.awesomekalin.jta.mod;
 
 import org.eu.awesomekalin.jta.mod.init.BlockEntityTypeInit;
 import org.eu.awesomekalin.jta.mod.init.BlockInit;
-import org.eu.awesomekalin.jta.mod.render.SimpleStationOperatorSignRender;
-import org.eu.awesomekalin.jta.mod.render.SimpleStationOperatorSignRenderWhite;
-import org.eu.awesomekalin.jta.mod.render.SimpleStationSignRender;
-import org.eu.awesomekalin.jta.mod.render.SimpleStationSignRenderWall;
+import org.eu.awesomekalin.jta.mod.render.*;
 import org.mtr.mapping.holder.RenderLayer;
 import org.mtr.mapping.holder.Style;
 import org.mtr.mapping.registry.RegistryClient;
@@ -67,6 +64,60 @@ public final class InitClient {
                         0,
                         0xFFFFFFFF,
                         true,
+                        Style.getDefaultFontIdMapped()
+                ));
+        REGISTRY_CLIENT.registerBlockEntityRenderer(
+                BlockEntityTypeInit.RUSTY_UK_BRITISH_OPERATOR_STATION_SIGN_BASE,
+                dispatcher -> new SimpleStationOperatorSignRustyRender<>(
+                        dispatcher,
+                        32 / 16F,
+                        0.2F / 8,
+                        0,
+                        0,
+                        .05F,
+                        0,
+                        0xFF0F0F0F,
+                        true,
+                        Style.getDefaultFontIdMapped()
+                ));
+        REGISTRY_CLIENT.registerBlockEntityRenderer(
+                BlockEntityTypeInit.RUSTY_UK_BRITISH_OPERATOR_STATION_SIGN_BASE_WHITE,
+                dispatcher -> new SimpleStationOperatorSignRustyRenderWhite<>(
+                        dispatcher,
+                        32 / 16F,
+                        0.2F / 8,
+                        0,
+                        0,
+                        .05F,
+                        0,
+                        0xFFFFFFFF,
+                        true,
+                        Style.getDefaultFontIdMapped()
+                ));
+        REGISTRY_CLIENT.registerBlockEntityRenderer(
+                BlockEntityTypeInit.WALL_UK_BRITISH_OPERATOR_STATION_SIGN_BASE,
+                dispatcher -> new SimpleStationOperatorSignWallRender<>(
+                        dispatcher,
+                        32 / 16F,
+                        0.2F / 8,
+                        0,
+                        0,
+                        -.4F,
+                        0,
+                        0xFF0F0F0F,
+                        Style.getDefaultFontIdMapped()
+                ));
+        REGISTRY_CLIENT.registerBlockEntityRenderer(
+                BlockEntityTypeInit.WALL_UK_BRITISH_OPERATOR_STATION_SIGN_BASE_WHITE,
+                dispatcher -> new SimpleStationOperatorSignWallRenderWhite<>(
+                        dispatcher,
+                        32 / 16F,
+                        0.2F / 8,
+                        0,
+                        0,
+                        -.4F,
+                        0,
+                        0xFFFFFFFF,
                         Style.getDefaultFontIdMapped()
                 ));
         REGISTRY_CLIENT.init();
--- fabric/src/main/java/org/eu/awesomekalin/jta/mod/init/BlockEntityTypeInit.java
+++ fabric/src/main/java/org/eu/awesomekalin/jta/mod/init/BlockEntityTypeInit.java
@@ -1,9 +1,7 @@
 package org.eu.awesomekalin.jta.mod.init;
 
 import org.eu.awesomekalin.jta.mod.Init;
-import org.eu.awesomekalin.jta.mod.blocks.directional.UKBritishOperatorStationSignBase;
-import org.eu.awesomekalin.jta.mod.blocks.directional.UKBritishRailStationSignSimple;
-import org.eu.awesomekalin.jta.mod.blocks.directional.UKBritishRailStationSignSimpleWall;
+import org.eu.awesomekalin.jta.mod.blocks.directional.*;
 import org.mtr.mapping.holder.Identifier;
 import org.mtr.mapping.registry.BlockEntityTypeRegistryObject;
 
@@ -43,6 +41,46 @@ public class BlockEntityTypeInit {
             UKBritishOperatorStationSignBase.TileEntityBritishRailOperatorSignWhite::new,
             BlockInit.AVANTI_WEST_COAST_STATION_SIGN::get,
             BlockInit.NORTHERN_STATION_SIGN::get
+    );
+
+    public static final BlockEntityTypeRegistryObject<UKBritishOperatorStationSignPoleBase.TileEntityBritishRailOperatorSign> RUSTY_UK_BRITISH_OPERATOR_STATION_SIGN_BASE = Init.REGISTRY.registerBlockEntityType(
+            new Identifier(
+                    Init.MOD_ID,
+                    "uk_british_operator_station_sign_base_rusty"
+            ),
+            UKBritishOperatorStationSignPoleBase.TileEntityBritishRailOperatorSign::new,
+            BlockInit.RUSTY_POLE_MERSEYRAIL_STATION_SIGN::get,
+            BlockInit.RUSTY_POLE_EMR_STATION_SIGN::get
+    );
+
+    public static final BlockEntityTypeRegistryObject<UKBritishOperatorStationSignPoleBase.TileEntityBritishRailOperatorSignWhite> RUSTY_UK_BRITISH_OPERATOR_STATION_SIGN_BASE_WHITE = Init.REGISTRY.registerBlockEntityType(
+            new Identifier(
+                    Init.MOD_ID,
+                    "uk_british_operator_station_sign_base_white_rusty"
+            ),
+            UKBritishOperatorStationSignPoleBase.TileEntityBritishRailOperatorSignWhite::new,
+            BlockInit.RUSTY_POLE_AVANTI_WEST_COAST_STATION_SIGN::get,
+            BlockInit.RUSTY_POLE_NORTHERN_STATION_SIGN::get
+    );
+
+    public static final BlockEntityTypeRegistryObject<UKBritishOperatorStationSignWallBase.TileEntityBritishRailOperatorSign> WALL_UK_BRITISH_OPERATOR_STATION_SIGN_BASE = Init.REGISTRY.registerBlockEntityType(
+            new Identifier(
+                    Init.MOD_ID,
+                    "uk_british_operator_station_sign_base_wall"
+            ),
+            UKBritishOperatorStationSignWallBase.TileEntityBritishRailOperatorSign::new,
+            BlockInit.WALL_MERSEYRAIL_STATION_SIGN::get,
+            BlockInit.WALL_EMR_STATION_SIGN::get
+    );
+
+    public static final BlockEntityTypeRegistryObject<UKBritishOperatorStationSignWallBase.TileEntityBritishRailOperatorSignWhite> WALL_UK_BRITISH_OPERATOR_STATION_SIGN_BASE_WHITE = Init.REGISTRY.registerBlockEntityType(
+            new Identifier(
+                    Init.MOD_ID,
+                    "uk_british_operator_station_sign_base_white_wall"
+            ),
+            UKBritishOperatorStationSignWallBase.TileEntityBritishRailOperatorSignWhite::new,
+            BlockInit.WALL_AVANTI_WEST_COAST_STATION_SIGN::get,
+            BlockInit.WALL_NORTHERN_STATION_SIGN::get
     );
     public static void INIT() {
         Init.LOGGER.debug("Registering JTA Block Entities");
