8eb42f83f617e76a06491a4965ead8a6b9eec36d
dashkt <contact.pear.casa@gmail.com>
2024-07-07 21:49:44 +0100

    closer to alpha 5 with vented electrical box and orange and green roadwork barriers

---
+++ fabric/src/main/java/org/eu/awesomekalin/jta/mod/blocks/directional/BritishVentedElectricalBox.java
@@ -0,0 +1,16 @@
+package org.eu.awesomekalin.jta.mod.blocks.directional;
+
+import org.eu.awesomekalin.jta.mod.blocks.DirectionalBlockExtension;
+import org.mtr.mapping.holder.*;
+import org.mtr.mapping.mapper.BlockHelper;
+
+public class BritishVentedElectricalBox extends DirectionalBlockExtension {
+    public BritishVentedElectricalBox() {
+        super(BlockHelper.createBlockSettings(false).strength(4.0f).nonOpaque().dynamicBounds());
+    }
+
+    @Override
+    public VoxelShape getOutlineShape2(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return BlockHelper.shapeUnion(Block.createCuboidShape(0, 0, 0, 16, 26, 8));
+    }
+}
