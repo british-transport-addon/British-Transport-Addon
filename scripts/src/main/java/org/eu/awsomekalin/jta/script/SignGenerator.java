33f796d98a80e18faf74add1ad291584a4e20b39
dashkt <contact.pear.casa@gmail.com>
2024-07-26 10:22:26 +0100

    remove fuc from SignGenerator script.

--- scripts/src/main/java/org/eu/awsomekalin/jta/script/SignGenerator.java
+++ scripts/src/main/java/org/eu/awsomekalin/jta/script/SignGenerator.java
@@ -106,7 +106,7 @@ public class SignGenerator {
         String jsonContenta = String.format(
                 "{\n" +
                         "  \"textures\": {\n" +
-                        "    \"sign\": \"jta:block/signs/" + keyReferences.get(parentType).replace("small", "simple")FUC + "/%s\",\n" +
+                        "    \"sign\": \"jta:block/signs/" + keyReferences.get(parentType).replace("small", "simple") + "/%s\",\n" +
                         "    \"sign_back\": \"jta:block/steel_rect\"\n" +
                         "  },\n" +
                         "  \"parent\": \"jta:block/sign/angles/%s_^angle^\"\n" +
