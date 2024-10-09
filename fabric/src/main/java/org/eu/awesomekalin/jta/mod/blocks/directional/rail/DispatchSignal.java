506d23c7c12d9f1acd8a2180214d32ccdc6308e3
dashkt <contact.pear.casa@gmail.com>
2024-10-09 16:48:44 +0100

    DispatchSignal logic control updates (this should now link to signals?)

--- fabric/src/main/java/org/eu/awesomekalin/jta/mod/blocks/directional/rail/DispatchSignal.java
+++ fabric/src/main/java/org/eu/awesomekalin/jta/mod/blocks/directional/rail/DispatchSignal.java
@@ -2,12 +2,18 @@ package org.eu.awesomekalin.jta.mod.blocks.directional.rail;
 
 import org.eu.awesomekalin.jta.mod.blocks.directional.DirectionalBlockEntityBase;
 import org.eu.awesomekalin.jta.mod.init.BlockEntityTypeInit;
-import org.mtr.mapping.holder.BlockPos;
-import org.mtr.mapping.holder.BlockState;
+import org.eu.awesomekalin.jta.mod.init.BlockInit;
+import org.jetbrains.annotations.Nullable;
+import org.mtr.mapping.holder.*;
 import org.mtr.mapping.mapper.BlockEntityExtension;
 import org.mtr.mapping.mapper.BlockHelper;
+import org.mtr.mod.block.BlockSignalBase;
 
 public class DispatchSignal extends DirectionalBlockEntityBase {
+
+    // Static variable to hold the currently selected BlockSignalBase position
+    private static BlockPos selectedSignalBasePosition = null;
+    public static Long LAST_UPDATE_TIME = System.currentTimeMillis();
 
     public DispatchSignal() {
         super(BlockHelper.createBlockSettings(false).strength(4.0f).nonOpaque().dynamicBounds());
@@ -15,17 +21,99 @@ public class DispatchSignal extends DirectionalBlockEntityBase {
 
     @Override
     public BlockEntityExtension createBlockEntity(BlockPos blockPos, BlockState blockState) {
-        return new TileEntityDispatchSignal(blockPos, blockState);
+        // Create the TileEntityDispatchSignal with the currently selected BlockSignalBase position
+        System.out.println(blockState.getBlock().getTranslationKey());
+        if (System.currentTimeMillis() < LAST_UPDATE_TIME + 2000L || selectedSignalBasePosition == null) return new TileEntityDispatchSignal(blockPos, blockState);
+        System.out.println(selectedSignalBasePosition.toShortString());
+
+        return new TileEntityDispatchSignal(
+                blockPos, blockState,
+                selectedSignalBasePosition.getX(),
+                selectedSignalBasePosition.getY(),
+                selectedSignalBasePosition.getZ()
+        );
+    }
+
+    @Override
+    public void onPlaced2(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
+        // Check if the block below is a BlockSignalBase
+        BlockPos blockBelow = pos.down();
+        BlockState blockBelowState = world.getBlockState(blockBelow);
+
+        if (PlayerEntity.isInstance(placer)) {
+            PlayerEntity player = PlayerEntity.cast(placer);
+
+            if (System.currentTimeMillis() < LAST_UPDATE_TIME + 2000L) {
+                DispatchSignal.TileEntityDispatchSignal entity = (DispatchSignal.TileEntityDispatchSignal) world.getBlockEntity(pos).data;
+                if (entity.signalX == 0 && entity.signalY == 0 && entity.signalZ == 0) {
+                    world.breakBlock(pos, false);
+                }
+                return;
+            }
+
+            LAST_UPDATE_TIME = System.currentTimeMillis();
+
+            if (selectedSignalBasePosition != null) {
+                player.sendMessage(Text.of("You have created a new DispatchSignal relative to BlockSignalBase at " + selectedSignalBasePosition.toShortString() + "!"), true);
+                selectedSignalBasePosition = null;
+                return;
+            }
+
+            // Ensure the block below is a BlockSignalBase
+            if (!(blockBelowState.getBlock().data instanceof BlockSignalBase)) {
+                player.sendMessage(Text.of("You need to click a BlockSignalBase before placing a DispatchSignal!"), true);
+                // Remove the DispatchSignal from the world if it fails
+                world.breakBlock(pos, false);
+                return;
+            }
+
+            // Set the selected position to the block below
+            selectedSignalBasePosition = blockBelow;
+            player.sendMessage(Text.of("You have selected this BlockSignalBase!"), true);
+            world.breakBlock(pos, false);
+            return;
+        }
+
+        // Create the TileEntityDispatchSignal with the selected BlockSignalBase position
+        super.onPlaced2(world, pos, state, placer, itemStack);
     }
 
     public static class TileEntityDispatchSignal extends BlockEntityExtension {
 
+        int signalX, signalY, signalZ;
+
         public TileEntityDispatchSignal(BlockPos pos, BlockState state) {
             super(BlockEntityTypeInit.DISPATCH_SIGNAL.get(), pos, state);
+            this.signalX = 0; // Default value
+            this.signalY = 0; // Default value
+            this.signalZ = 0; // Default value
+        }
+
+        public TileEntityDispatchSignal(BlockPos pos, BlockState state, int signalX, int signalY, int signalZ) {
+            super(BlockEntityTypeInit.DISPATCH_SIGNAL.get(), pos, state);
+            this.signalX = signalX;
+            this.signalY = signalY;
+            this.signalZ = signalZ;
+        }
+
+        @Override
+        public void writeCompoundTag(CompoundTag compoundTag) {
+            super.writeCompoundTag(compoundTag);
+            compoundTag.putInt("signalX", signalX);
+            compoundTag.putInt("signalY", signalY);
+            compoundTag.putInt("signalZ", signalZ);
+        }
+
+        @Override
+        public void readCompoundTag(CompoundTag compoundTag) {
+            super.readCompoundTag(compoundTag);
+            this.signalX =  compoundTag.getInt("signalX");
+            this.signalY =  compoundTag.getInt("signalY");
+            this.signalZ =  compoundTag.getInt("signalZ");
         }
 
         public boolean shouldRender() {
             return true;
         }
-    }//Yes! Spam!
+    }
 }
