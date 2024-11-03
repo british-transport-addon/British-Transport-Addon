d0c0192ae1e7f36ba660079c032a822671a3cc09
dashkt <contact.pear.casa@gmail.com>
2024-11-03 09:17:58 +0000

    depo depo

--- fabric/src/main/java/org/eu/awesomekalin/jta/mod/init/SoundInit.java
+++ fabric/src/main/java/org/eu/awesomekalin/jta/mod/init/SoundInit.java
@@ -6,12 +6,7 @@ import org.mtr.mapping.registry.SoundEventRegistryObject;
 
 public class SoundInit {
     public static final SoundEventRegistryObject ELECTRICAL_HUM_1 = Init.REGISTRY.registerSoundEvent(new Identifier(Init.MOD_ID, "electrical_hum_1"));
-    public static final SoundEventRegistryObject AXON_BODY_3 = Init.REGISTRY.registerSoundEvent(new Identifier(Init.MOD_ID, "axon_body_3"));
-    public static final SoundEventRegistryObject MET_POLICE_RADIO_IN = Init.REGISTRY.registerSoundEvent(new Identifier(Init.MOD_ID, "met_police_radio_in"));
-    public static final SoundEventRegistryObject MET_POLICE_RADIO_INCOMING = Init.REGISTRY.registerSoundEvent(new Identifier(Init.MOD_ID, "met_police_radio_incoming"));
-    public static final SoundEventRegistryObject MET_POLICE_RADIO_OUT = Init.REGISTRY.registerSoundEvent(new Identifier(Init.MOD_ID, "met_police_radio_out"));
-    public static final SoundEventRegistryObject MET_POLICE_RADIO_INTERACT = Init.REGISTRY.registerSoundEvent(new Identifier(Init.MOD_ID, "met_police_radio_interact"));
-    public static void INIT() {
+   public static void INIT() {
         Init.LOGGER.debug("Registering JTA Sound Events.");
     }
 }
