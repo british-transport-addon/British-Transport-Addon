9015eee3fc557498000c289619b15cb02930c372
dashkt <contact.pear.casa@gmail.com>
2025-03-10 05:44:59 +0000

    display fix big

--- fabric/src/main/java/org/eu/awesomekalin/jta/mod/render/rail/signal/RenderCrossingSignal.java
+++ fabric/src/main/java/org/eu/awesomekalin/jta/mod/render/rail/signal/RenderCrossingSignal.java
@@ -20,20 +20,20 @@ public class RenderCrossingSignal<T extends BlockSignalBase.BlockEntityBase> ext
 
     @Override
     protected void render(StoredMatrixTransformations storedMatrixTransformations, T entity, float tickDelta, int occupiedAspect, boolean isBackSide) {
-        final float xOffset = 0.2F; // Offset from center
+        final float xOffset = 0.4F; // Offset from center
         final int activeColor = 0xFFFF0000; // Red (On)
         final int inactiveColor = 0xFF222222; // Dim (Off)
 
         boolean isSignalOn = occupiedAspect > 0; // If active, turn lights on
         boolean isFlashing = (System.currentTimeMillis() / 500) % 2 == 0; // Flash every 500ms
 
-        MainRenderer.scheduleRender(new Identifier(Init.MOD_ID, "textures/block/red.png"), false, QueuedRenderLayer.LIGHT, (graphicsHolder, offset) -> {
+        MainRenderer.scheduleRender(new Identifier(Init.MOD_ID, "textures/block/digital_signal.png"), false, QueuedRenderLayer.LIGHT, (graphicsHolder, offset) -> {
             storedMatrixTransformations.transform(graphicsHolder, offset);
 
             int leftDisplayColor = (isSignalOn && isFlashing) ? activeColor : inactiveColor;
             int rightDisplayColor = (isSignalOn && !isFlashing) ? activeColor : inactiveColor;
-            float y = 0.25F; // Fixed position
-            float size = 0.06F; // Square size
+            float y = .5F; // Fixed position
+            float size = 0.2F; // Square size
 
             // Left light
             IDrawing.drawTexture(graphicsHolder, -xOffset - size, y - size, -0.19375F,
