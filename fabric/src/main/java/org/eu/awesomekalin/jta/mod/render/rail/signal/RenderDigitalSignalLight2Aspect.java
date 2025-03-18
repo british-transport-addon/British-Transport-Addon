6860ce637cee7acaf4ba769ec2c40a93c6ddf969
dashkt <contact.pear.casa@gmail.com>
2025-03-18 00:53:44 +0000

    fix height

--- fabric/src/main/java/org/eu/awesomekalin/jta/mod/render/rail/signal/RenderDigitalSignalLight2Aspect.java
+++ fabric/src/main/java/org/eu/awesomekalin/jta/mod/render/rail/signal/RenderDigitalSignalLight2Aspect.java
@@ -26,7 +26,7 @@ public class RenderDigitalSignalLight2Aspect<T extends BlockSignalBase.BlockEnti
 		int topColor;
 		int bottomColor;
 
-		float topY = 0.3375F;
+		float topY = 0.2F;
 		float bottomY = 0.0625F;
 
 		switch (occupiedAspect) {
